cc47fddd3667fbc5fd364d996c583b1d15aa9fec
Bilge Taylan Ulusoy <bilge.ulusoy@me.com>
2015-09-11 11:27:06 +0200

    Implemented -URLSession:task:didReceiveChallenge:completionHandler: in PINURLSessionManager

--- Pod/Classes/PINURLSessionManager.m
+++ Pod/Classes/PINURLSessionManager.m
@@ -67,6 +67,19 @@
 
 #pragma mark NSURLSessionDataDelegate
 
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * __nullable credential))completionHandler {
+	[self lock];
+	dispatch_queue_t delegateQueue = self.delegateQueues[@(task.taskIdentifier)];
+	[self unlock];
+	
+	__weak typeof(self) weakSelf = self;
+	dispatch_async(delegateQueue, ^{
+		if ([weakSelf.delegate respondsToSelector:@selector(didReceiveAuthenticationChallenge:forTask:completionHandler:)]) {
+			[weakSelf.delegate didReceiveAuthenticationChallenge:challenge forTask:task completionHandler:completionHandler];
+		}
+	});
+}
+
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
 {
     [self lock];
