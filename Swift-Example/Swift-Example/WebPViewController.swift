b2408ce80e4d92893f446030ead206c9a58e7fa8
Marius Landwehr <marius.landwehr@gmail.com>
2016-01-23 23:25:37 +0100

    Added Example ViewControllers for degraded, processing, progressive, scrolling and WebP

---
+++ Swift-Example/Swift-Example/WebPViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebPViewController.swift
+//  Swift-Example
+//
+//  Created by Marius Landwehr on 23.01.16.
+//  Copyright © 2016 Marius Landwehr. All rights reserved.
+//
+
+import UIKit
+
+class WebPViewController: UIViewController {
+
+}
