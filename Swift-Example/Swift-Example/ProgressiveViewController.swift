f3f56d355b81518bb80c58364155147b988910d3
Marius Landwehr <marius.landwehr@gmail.com>
2016-02-03 21:40:51 +0100

    added Progressive example

--- Swift-Example/Swift-Example/ProgressiveViewController.swift
+++ Swift-Example/Swift-Example/ProgressiveViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import PINRemoteImage
+import PINCache
 
 class ProgressiveViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imageView.pin_updateWithProgress = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +26,15 @@ class ProgressiveViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let imageURL = NSURL(string: "https://s-media-cache-ak0.pinimg.com/1200x/2e/0c/c5/2e0cc5d86e7b7cd42af225c29f21c37f.jpg") {
+            PINRemoteImageManager.sharedImageManager().setProgressThresholds([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], completion: nil)
+            PINRemoteImageManager.sharedImageManager().cache.removeObjectForKey(PINRemoteImageManager.sharedImageManager().cacheKeyForURL(imageURL, processorKey: ""))
+            
+            imageView.pin_setImageFromURL(imageURL)
+        }
     }
-    */
 
 }
