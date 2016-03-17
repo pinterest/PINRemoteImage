4fbb5b540c82b5f871d8e3431d0eadbffce3faeb
Garrett Moon <garrett@pinterest.com>
2016-03-17 10:40:38 -0700

    Spacing

--- Pod/Classes/PINRemoteImageManager.m
+++ Pod/Classes/PINRemoteImageManager.m
@@ -232,7 +232,7 @@ static dispatch_once_t sharedDispatchToken;
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         typeof(self) strongSelf = weakSelf;
         [strongSelf lock];
-        strongSelf.authenticationChallengeHandler = challengeBlock;
+            strongSelf.authenticationChallengeHandler = challengeBlock;
         [strongSelf unlock];
     });
 }
