8c6ffcb3eee8a031a29b75363fc719a8732812ee
Garrett Moon <garrett@pinterest.com>
2016-11-11 10:15:15 -0800

    Move to run_tests_update_status.sh

--- run_tests_update_status.sh
+++ run_tests_update_status.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+if [[ -z "${UPDATE_STATUS_PATH}" || -z "${BUILDKITE_PULL_REQUEST}" || -z "${BUILDKITE_BUILD_URL}" ]] ; then
+    echo "Update status path, build url or pull request unset."
+    trap - EXIT
+    exit 255
+fi
+
 trapped="false"
 function trap_handler() {
     if [[ "$trapped" = "false" ]]; then
-        echo "Tests failed, updating status to failure"
+        ${UPDATE_STATUS_PATH} pinterest PINRemoteImage ${BUILDKIT_PULL_REQUEST} failure ${BUILDKITE_BUILD_URL} "Tests failed." "CI/Pinterest"
+        echo "Tests failed, updated status to failure"
     fi
     trapped="true"
 }
@@ -12,6 +19,7 @@ trap trap_handler INT TERM EXIT
 
 ./build.sh all
 
-echo "All tests succeeded, updating status to success"
+${UPDATE_STATUS_PATH} pinterest PINRemoteImage ${BUILDKIT_PULL_REQUEST} success ${BUILDKITE_BUILD_URL} "Tests passed." "CI/Pinterest"
+echo "All tests succeeded, updated status to success"
 trap - EXIT
 exit 0
