95272711e652460efada99bcdff147f7d94d3c61
Garrett Moon <garrett@pinterest.com>
2016-11-11 11:19:25 -0800

    Maybe this works?

--- run_tests_update_status.sh
+++ run_tests_update_status.sh
@@ -14,7 +14,7 @@ fi
 trapped="false"
 function trap_handler() {
     if [[ "$trapped" = "false" ]]; then
-        ${UPDATE_STATUS_PATH} pinterest PINRemoteImage ${BUILDKIT_PULL_REQUEST} failure ${BUILDKITE_BUILD_URL} "Tests failed." "CI/Pinterest"
+        ${UPDATE_STATUS_PATH} "pinterest" "PINRemoteImage" "${BUILDKIT_PULL_REQUEST}" "failure" "${BUILDKITE_BUILD_URL}" "Tests failed." "CI/Pinterest"
         echo "Tests failed, updated status to failure"
     fi
     trapped="true"
@@ -23,7 +23,7 @@ trap trap_handler INT TERM EXIT
 
 ./build.sh all
 
-${UPDATE_STATUS_PATH} pinterest PINRemoteImage ${BUILDKIT_PULL_REQUEST} success ${BUILDKITE_BUILD_URL} "Tests passed." "CI/Pinterest"
+${UPDATE_STATUS_PATH} "pinterest" "PINRemoteImage" ${BUILDKIT_PULL_REQUEST} "success" ${BUILDKITE_BUILD_URL} "Tests passed." "CI/Pinterest"
 echo "All tests succeeded, updated status to success"
 trap - EXIT
 exit 0
