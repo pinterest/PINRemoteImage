5aaf21323502c7172b991c5435a5b6a8c36c230d
Garrett Moon <garrett@pinterest.com>
2016-11-14 14:45:51 -0800

    Add Carthage to tests and improve build script.

--- build.sh
+++ build.sh
@@ -7,14 +7,32 @@ SDK="iphonesimulator10.1"
 
 MODE="$1"
 
+function echoHeader() {
+    echo ""
+    echo ""
+    echo $1
+    echo ""
+}
+
 if [[ -z "$MODE" ]] ; then
     MODE="all"
 fi
 
 if [[ "$MODE" = "lint" || "$MODE" = "all" ]] ; then
+    echoHeader "Testing CocoaPod configurations"
     pod lib lint --allow-warnings
 fi
 
 if [[ "$MODE" = "tests" || "$MODE" = "all" ]] ; then
+    echoHeader "Running unit tests"
     xcodebuild clean test -destination "$PLATFORM" -sdk "$SDK" -workspace Example/PINRemoteImage.xcworkspace -scheme PINRemoteImage ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO | xcpretty -t; test ${PIPESTATUS[0]} -eq 0
 fi
+
+if [[ "$MODE" = "carthage" || "$MODE" = "all" ]] ; then
+    echoHeader "Testing Carthage build"
+    carthage update
+    carthage build --no-skip-current --project-directory PINRemoteImage
+fi
+
+echo ""
+echo "Finished tests"
