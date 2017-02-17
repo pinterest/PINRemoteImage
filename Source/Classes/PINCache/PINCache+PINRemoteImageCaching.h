d0690e98df814090960fb58f9df3254ef6f43b42
Garrett Moon <garrett@pinterest.com>
2017-02-17 14:31:39 -0800

    This wasn't meant to be included in this patch.

--- Source/Classes/PINCache/PINCache+PINRemoteImageCaching.h
+++ Source/Classes/PINCache/PINCache+PINRemoteImageCaching.h
@@ -7,16 +7,8 @@
 //
 
 #import <PINCache/PINCache.h>
-#import <PINRemoteImage/PINRemoteImageManager.h>
-
 #import "PINRemoteImageCaching.h"
 
 @interface PINCache (PINRemoteImageCaching) <PINRemoteImageCaching>
-
-@end
-
-@interface PINRemoteImageManager (PINCache)
-
-@property (nonatomic, readonly, nonnull) PINCache *cache;
 
 @end
