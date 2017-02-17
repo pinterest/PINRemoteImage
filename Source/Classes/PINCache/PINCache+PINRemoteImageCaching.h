098fe705c61bdee2a508d6be64012e0d428c4919
Garrett Moon <garrett@pinterest.com>
2017-02-17 14:31:06 -0800

    Expose PINCache

--- Source/Classes/PINCache/PINCache+PINRemoteImageCaching.h
+++ Source/Classes/PINCache/PINCache+PINRemoteImageCaching.h
@@ -7,8 +7,16 @@
 //
 
 #import <PINCache/PINCache.h>
+#import <PINRemoteImage/PINRemoteImageManager.h>
+
 #import "PINRemoteImageCaching.h"
 
 @interface PINCache (PINRemoteImageCaching) <PINRemoteImageCaching>
+
+@end
+
+@interface PINRemoteImageManager (PINCache)
+
+@property (nonatomic, readonly, nonnull) PINCache *cache;
 
 @end
