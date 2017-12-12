f3199e5c2c7904476788312d54ce370030d0d7eb
Shen Lu <shen.lu@gotinder.com>
2017-12-12 15:27:23 -0800

    Exposed URL Response in PINRemoteImageManagerResult objects for downloading images to show http code (if available)

--- Pod/Classes/PINRemoteImageTask.h
+++ Pod/Classes/PINRemoteImageTask.h
@@ -23,7 +23,7 @@
                   progressDownloadBlock:(nullable PINRemoteImageManagerProgressDownload)progressDownloadBlock
                                withUUID:(nonnull NSUUID *)UUID;
 - (void)removeCallbackWithUUID:(nonnull NSUUID *)UUID;
-- (void)callCompletionsWithQueue:(nonnull dispatch_queue_t)queue remove:(BOOL)remove withImage:(nullable PINImage *)image animatedImage:(nullable FLAnimatedImage *)animatedImage cached:(BOOL)cached error:(nullable NSError *)error;
+- (void)callCompletionsWithQueue:(nonnull dispatch_queue_t)queue remove:(BOOL)remove withImage:(nullable PINImage *)image animatedImage:(nullable FLAnimatedImage *)animatedImage response:(NSURLResponse *)response cached:(BOOL)cached error:(nullable NSError *)error;
 //returns YES if no more attached completionBlocks
 - (BOOL)cancelWithUUID:(nonnull NSUUID *)UUID manager:(nullable PINRemoteImageManager *)manager;
 - (void)setPriority:(PINRemoteImageManagerPriority)priority;
