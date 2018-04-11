2b6e37ac429fafe9d14cd5583482d2ad340bd63a
Garrett Moon <garrett@pinterest.com>
2018-04-11 11:49:01 -0700

    Progressive rendering was broken in the swift example.

--- Examples/Swift-Example/Swift-Example/AppDelegate.swift
+++ Examples/Swift-Example/Swift-Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PINRemoteImage
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        PINRemoteImageManager.shared().setProgressiveRendersMaxProgressiveRenderSize(CGSize.init(width: 2048, height: 2048), completion: nil)
         return true
     }
 
