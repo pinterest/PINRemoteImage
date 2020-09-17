dab6a3c1749006d1caade4f3755e0c672a20d855
IHEARTCOOKIES <filipbusicverizon@gmail.com>
2020-09-17 14:25:16 -0700

    Integrated UIGraphicsImageRenderer for iOS/tvOS 10.0 and above (#549)
    * Moved to usage of UIGraphicsImageRenderer for iOS 10 and above
    
    * Added tests to exercise UIGraphicsImageRenderer behavior and internal implementation.
    
    * Added additional availability flags and corrected typo
    
    * Configured availability flags for iOS/tvOS
    
    * Added inline and moved to the top of the file

--- Source/Classes/Categories/PINImage+DecodedImage.m
+++ Source/Classes/Categories/PINImage+DecodedImage.m
@@ -16,6 +16,49 @@
 
 #import "NSData+ImageDetectors.h"
 
+NS_INLINE BOOL pin_CGImageRefIsOpaque(CGImageRef imageRef) {
+    CGImageAlphaInfo alpha = CGImageGetAlphaInfo(imageRef);
+    switch (alpha) {
+        case kCGImageAlphaNone:
+        case kCGImageAlphaNoneSkipLast:
+        case kCGImageAlphaNoneSkipFirst:
+            return YES;
+        default:
+            return NO;
+    }
+}
+
+#if PIN_TARGET_IOS
+NS_INLINE void pin_degreesFromOrientation(UIImageOrientation orientation, void (^completion)(CGFloat degrees, BOOL horizontalFlip, BOOL verticalFlip)) {
+    switch (orientation) {
+        case UIImageOrientationUp: // default orientation
+            completion(0.0, NO, NO);
+            break;
+        case UIImageOrientationDown: // 180 deg rotation
+            completion(180.0, NO, NO);
+            break;
+        case UIImageOrientationLeft:
+            completion(270.0, NO, NO); // 90 deg CCW
+            break;
+        case UIImageOrientationRight:
+            completion(90.0, NO, NO); // 90 deg CW
+            break;
+        case UIImageOrientationUpMirrored: // as above but image mirrored along other axis. horizontal flip
+            completion(0.0, YES, NO);
+            break;
+        case UIImageOrientationDownMirrored: // horizontal flip
+            completion(180.0, YES, NO);
+            break;
+        case UIImageOrientationLeftMirrored: // vertical flip
+            completion(270.0, NO, YES);
+            break;
+        case UIImageOrientationRightMirrored: // vertical flip
+            completion(90.0, NO, YES);
+            break;
+    }
+}
+#endif
+
 #if !PIN_TARGET_IOS
 @implementation NSImage (PINiOSMapping)
 
@@ -126,23 +169,76 @@ NSData * __nullable PINImagePNGRepresentation(PINImage * __nonnull image) {
 {
 #endif
 #if PIN_TARGET_IOS
-    return [UIImage imageWithCGImage:[self pin_decodedImageRefWithCGImageRef:imageRef] scale:1.0 orientation:orientation];
+    if (@available(iOS 10.0, tvOS 10.0, *)) {
+        return [self pin_decodedImageUsingGraphicsImageRendererRefWithCGImageRef:imageRef scale:1.0 orientation:orientation];
+    } else {
+        return [UIImage imageWithCGImage:[self pin_decodedImageRefWithCGImageRef:imageRef] scale:1.0 orientation:orientation];
+    }
 #elif PIN_TARGET_MAC
     return [[NSImage alloc] initWithCGImage:[self pin_decodedImageRefWithCGImageRef:imageRef] size:NSZeroSize];
 #endif
 }
 
-+ (CGImageRef)pin_decodedImageRefWithCGImageRef:(CGImageRef)imageRef
-{
-    BOOL opaque = YES;
-    CGImageAlphaInfo alpha = CGImageGetAlphaInfo(imageRef);
-    if (alpha == kCGImageAlphaFirst || alpha == kCGImageAlphaLast || alpha == kCGImageAlphaOnly || alpha == kCGImageAlphaPremultipliedFirst || alpha == kCGImageAlphaPremultipliedLast) {
-        opaque = NO;
+#if PIN_TARGET_IOS
++ (PINImage *)pin_decodedImageUsingGraphicsImageRendererRefWithCGImageRef:(CGImageRef)imageRef
+                                                                    scale:(CGFloat)scale
+                                                              orientation:(UIImageOrientation)orientation API_AVAILABLE(ios(10.0), tvos(10.0)) {
+    UIGraphicsImageRendererFormat *format = nil;
+    if (@available(iOS 11.0, tvOS 11.0, *)) {
+        format = [UIGraphicsImageRendererFormat preferredFormat];
+    } else {
+        format = [UIGraphicsImageRendererFormat defaultFormat];
     }
     
+    format.scale = scale;
+    format.opaque = pin_CGImageRefIsOpaque(imageRef);
+    
+    __block CGFloat radians = 0.0;
+    __block BOOL doHorizontalFlip = NO;
+    __block BOOL doVerticalFlip = NO;
+    
+    pin_degreesFromOrientation(orientation, ^(CGFloat degrees, BOOL horizontalFlip, BOOL verticalFlip) {
+        // Convert degrees to radians
+        radians = [[[NSMeasurement alloc] initWithDoubleValue:degrees
+                                                         unit:[NSUnitAngle degrees]]
+                   measurementByConvertingToUnit:[NSUnitAngle radians]].doubleValue;
+        doHorizontalFlip = horizontalFlip;
+        doVerticalFlip = verticalFlip;
+    });
+    
+    // Create rotation out of radians
+    CGAffineTransform transform = CGAffineTransformMakeRotation(radians);
+    
+    // Grab image size
     CGSize imageSize = CGSizeMake(CGImageGetWidth(imageRef), CGImageGetHeight(imageRef));
     
-    CGBitmapInfo info = opaque ? (kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Host) : (kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
+    // Rotate rect by transformation
+    CGRect rotatedRect = CGRectApplyAffineTransform(CGRectMake(0.0, 0.0, imageSize.width, imageSize.height), transform);
+    
+    // Use graphics renderer to render image
+    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:rotatedRect.size format:format];
+    
+    return [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+        CGContextRef ctx = rendererContext.CGContext;
+        
+        // Flip the default coordinate system for iOS/tvOS:  https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW4
+        CGContextTranslateCTM(ctx, rotatedRect.size.width / 2.0, rotatedRect.size.height / 2.0);
+        CGContextScaleCTM(ctx, (doHorizontalFlip ? -1.0 : 1.0), (doVerticalFlip ? 1.0 : -1.0));
+        
+        // Apply transformation
+        CGContextConcatCTM(ctx, transform);
+        
+        // Draw image
+        CGContextDrawImage(ctx, CGRectMake(-(imageSize.width / 2.0), -(imageSize.height / 2.0), imageSize.width, imageSize.height), imageRef);
+    }];
+}
+#endif
+
++ (CGImageRef)pin_decodedImageRefWithCGImageRef:(CGImageRef)imageRef
+{
+    CGSize imageSize = CGSizeMake(CGImageGetWidth(imageRef), CGImageGetHeight(imageRef));
+    
+    CGBitmapInfo info = pin_CGImageRefIsOpaque(imageRef) ? (kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Host) : (kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
     CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
     
     //Use UIGraphicsBeginImageContext parameters from docs: https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIKitFunctionReference/#//apple_ref/c/func/UIGraphicsBeginImageContextWithOptions
