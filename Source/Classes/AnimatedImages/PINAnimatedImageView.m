pinterest/PINRemoteImage

commit b8a2e05948a6e0bcffe5f261a6bc1472fd85ec9e

Conditionally call CADisplayLink method based upon iOS10 availability to remove deprecation warning (#552)
* Conditionally call CADisplayLink method based upon iOS10 availability to remove deprecation warning.

* guard for tvos too

* add PIN_TARGET_IOS to protect where macos doesn't have this update yet

Source/Classes/AnimatedImages/PINAnimatedImageView.m
@@ -225,7 +225,15 @@
     if (_displayLink == nil) {
         _playHead = 0;
         _displayLink = [PINDisplayLink displayLinkWithTarget:[PINRemoteWeakProxy weakProxyWithTarget:self] selector:@selector(displayLinkFired:)];
-        _displayLink.frameInterval = frameInterval;
+#if PIN_TARGET_IOS
+        if (@available(iOS 10.0, tvOS 10.0, *)) {
+            _displayLink.preferredFramesPerSecond = frameInterval;
+        } else {
+#endif
+            _displayLink.frameInterval = frameInterval;
+#if PIN_TARGET_IOS
+        }
+#endif
         _lastSuccessfulFrameIndex = NSUIntegerMax;
         
         [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:self.animatedImageRunLoopMode];

commit 9dcf003939e7116abaed7adee4106dcfee82159c

Use proper units when setting preferredFramesPerSecond
preferredFramesPerSecond expects frames-per-second, rather than the
fractional interval expected by frameInterval.

For example, frameInterval=2 is the equivalent (on most devices) of
preferredFramesPerSecond=30.

This issue was introduced in https://github.com/pinterest/PINRemoteImage/pull/552

Source/Classes/AnimatedImages/PINAnimatedImageView.m
@@ -221,13 +221,16 @@
     
     // Get frame interval before holding display link lock to avoid deadlock
     NSUInteger frameInterval = self.animatedImage.frameInterval;
+    
+    // Convert from display link fractional value to fps (note: frameInterval is always at least 1)
+    NSInteger frameRate = ceil([PINAnimatedImage maximumFramesPerSecond] / ((double) frameInterval));
 
     if (_displayLink == nil) {
         _playHead = 0;
         _displayLink = [PINDisplayLink displayLinkWithTarget:[PINRemoteWeakProxy weakProxyWithTarget:self] selector:@selector(displayLinkFired:)];
 #if PIN_TARGET_IOS
         if (@available(iOS 10.0, tvOS 10.0, *)) {
-            _displayLink.preferredFramesPerSecond = frameInterval;
+            _displayLink.preferredFramesPerSecond = frameRate;
         } else {
 #endif
             _displayLink.frameInterval = frameInterval;