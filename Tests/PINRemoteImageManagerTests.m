8ea32d6f3155f30d67ac57f4f742e4d88361e84e
rruizmurguia <rruizmurguia@pinterest.com>
2022-01-28 10:44:06 -0600

    New test that checks old implementation and new one return the same key

---
+++ Tests/PINRemoteImageManagerTests.m
@@ -0,0 +1,54 @@
+//
+//  PINRemoteImageManagerTests.m
+//  
+//
+//  Created by Rodrigo Ruiz Murguia on 28/01/22.
+//
+
+#import <PINRemoteImage/PINRemoteImageManager.h>
+#import <XCTest/XCTest.h>
+
+@interface PINRemoteImageManager (TestSecrets)
+
+-(NSString *) hashCacheKey_removeMeIOS13:(NSString *) string;
+
+@end
+
+@interface PINRemoteImageManagerTests : XCTestCase
+
+@end
+
+@implementation PINRemoteImageManagerTests
+
+- (void)setUp
+{
+}
+
+- (void)tearDown
+{
+}
+
+- (void)testCCMD5AndCryptoKitCacheKeyGenerateSameKey
+{
+    PINRemoteImageManager *manager = [PINRemoteImageManager sharedImageManager];
+
+    int num = 250;
+    NSMutableString *bigString = [NSMutableString stringWithCapacity:num];
+    for (int i = 0; i < num; i++) {
+        [bigString appendFormat:@"%C", (unichar)('a')];
+    }
+
+    NSString *urlString = [@"http://www.mycooldomain.com/" stringByAppendingString:bigString];
+    NSArray<NSString* >* deviations = @[ @"/api", @"/home", @"/play" ];
+
+    for(NSString* deviation in deviations) {
+        NSString* composedString = [urlString stringByAppendingString:deviation];
+        NSString* ccmd5Key = [manager hashCacheKey_removeMeIOS13:composedString];
+        NSString* cryptoKitKey = [composedString cryptoKitCacheKeyMD5];
+
+        NSString* error = [NSString stringWithFormat: @"%@%@%@", @"Hashes of: ", composedString, @" do not match"];
+        XCTAssertEqualObjects(ccmd5Key, cryptoKitKey, @"%@", error);
+    }
+}
+
+@end
