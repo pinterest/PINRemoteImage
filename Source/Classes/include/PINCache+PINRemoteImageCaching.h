a66de4b63dd39eeba3f7b72bcfc8035f01a6d9ff
Maxwell Elliott <maxwell.elliott@gotinder.com>
2022-08-18 10:34:26 -0400

    More imports

--- Source/Classes/include/PINCache+PINRemoteImageCaching.h
+++ Source/Classes/include/PINCache+PINRemoteImageCaching.h
@@ -9,7 +9,7 @@
 #if SWIFT_PACKAGE
 @import PINCache;
 #else
-#import "external/PINCache/Source/PINCache.h"
+#import "../PINCache/Source/PINCache.h"
 #endif
 
 #import "Source/Classes/include/PINRemoteImageCaching.h"
